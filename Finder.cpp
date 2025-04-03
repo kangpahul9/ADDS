a2b14b211dbc7b64d2ace9a685d6c5f1b63c3d45
kangpahul9 <pahulpreet2959@gmail.com>
2025-04-03 20:58:30 +1030

    3

---
+++ Finder.cpp
@@ -0,0 +1,19 @@
+#include "Finder.h"
+using namespace std;
+
+vector<int> Finder::findSubstrings(string s1, string s2) {
+    vector<int> result;
+    size_t last_found_index = 0;  
+    for (size_t i = 1; i <= s2.size(); i++) {
+        size_t found = s1.find(s2.substr(0, i), last_found_index); 
+
+        if (found != string::npos) {
+            result.push_back(found);
+            last_found_index = found + 1;  
+        } else {
+            result.push_back(-1); 
+            break;  
+        }
+    }
+    return result;
+}
