8d03007d28bcb1c154436e651c459724246e990d
kangpahul9 <pahulpreet2959@gmail.com>
2025-04-03 21:14:39 +1030

    4

--- Finder.cpp
+++ Finder.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
-    size_t last_found_index = 0;
+    int last_found_index = 0;
 
     for (size_t i = 1; i <= s2.size(); i++) {
         size_t found = s1.find(s2.substr(0, i), last_found_index);
