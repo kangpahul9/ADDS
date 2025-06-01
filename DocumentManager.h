182624dec2e05f5d0fccc5e64a19845c81dd01ca
kangpahul9 <pahulpreet2959@gmail.com>
2025-06-01 21:38:27 +0930

    2

--- DocumentManager.h
+++ DocumentManager.h
@@ -14,8 +14,8 @@ class DocumentManager {
     public:
         void addDocument(std::string name, int id, int license_limit);
         void addPatron(int patronID);
-        int search(std::string name);  // returns docid if name is in the document collection or 0 if the name is not in the collection
-        bool borrowDocument(int docid, int patronID);  // returns true if document is borrowed, false if it can not be borrowed (invalid patronid or the number of copies current borrowed has reached the license limit)
+        int search(std::string name);  
+        bool borrowDocument(int docid, int patronID);
         void returnDocument(int docid, int patronID);
 };
 
